d8b606d4303a207b4a646cffd75c7c77d0a5e975
Paul Gross <pauligross@gmail.com>
2023-06-26 17:56:16 +1000

    Experimental socket server.

--- app/src/main/java/game/paulgross/tictactoe/MainActivity.kt
+++ app/src/main/java/game/paulgross/tictactoe/MainActivity.kt
@@ -8,8 +8,11 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import java.io.OutputStream
 import java.net.ServerSocket
 import java.net.Socket
+import java.nio.charset.Charset
+import java.util.Scanner
 
 class MainActivity : AppCompatActivity() {
 
@@ -334,6 +337,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     class ClientHandler(client: Socket) {
+        private val client: Socket = client
+        private val reader: Scanner = Scanner(client.getInputStream())
+        private val writer: OutputStream = client.getOutputStream()
 
+        private var running: Boolean = false
+        fun run() {
+            running = true
+            // Indicate the protocol capabilities and version
+            write("Connection now open to the server.\n")
+
+            while (running) {
+                try {
+                    val text = reader.nextLine()
+                    if (text == "PROTOCOL:CLOSE") {
+                        shutdown()
+                        continue
+                    }
+
+                    // TODO - Queue the request to the appropriate handler.
+                } catch (ex: Exception) {
+                    // TODO: Implement exception handling
+                    shutdown()
+                } finally {
+
+                }
+            }
+        }
+
+        private fun write(message: String) {
+            writer.write((message + '\n').toByteArray(Charset.defaultCharset()))
+        }
+
+        private fun shutdown() {
+            running = false
+            client.close()
+            println("${client.inetAddress.hostAddress} closed the connection")
+        }
     }
 }
